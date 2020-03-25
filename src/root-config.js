873dcd774c0050d8c24ccae5f12b51d8ee131de7
Danilo Peña Mac <nicolasdpf@gmail.com>
2020-03-25 16:00:44 -0500

    Weather widget successfully added

--- src/root-config.js
+++ src/root-config.js
@@ -1,8 +1,21 @@
 import * as singleSpa from "single-spa";
+import "zone.js";
 
 singleSpa.registerApplication(
   "micro-frontends-layout",
   () => import("micro-frontends-layout"),
+  location => location.pathname.startsWith("/")
+);
+
+singleSpa.registerApplication(
+  "todolist",
+  () => import("micro-frontends-todolist"),
+  location => location.pathname.startsWith("/")
+);
+
+singleSpa.registerApplication(
+  "angularapp",
+  () => import("micro-frontends-weather"),
   location => location.pathname.startsWith("/")
 );
 
