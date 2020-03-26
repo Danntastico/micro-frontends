28b3936d51232c6318ec0f4e61cd2b34d3a268ff
Danilo Peña Mac <nicolasdpf@gmail.com>
2020-03-26 15:46:20 -0500

    adding New routes and login components

--- src/root-config.js
+++ src/root-config.js
@@ -1,22 +1,33 @@
 import * as singleSpa from "single-spa";
 import "zone.js";
 
+function appFilter(routes) {
+  return function(location) {
+    return routes.some(route => location.pathname === route);
+  };
+}
+
 singleSpa.registerApplication(
   "micro-frontends-layout",
   () => import("micro-frontends-layout"),
-  location => location.pathname.startsWith("/")
+  appFilter(["/"])
 );
 
 singleSpa.registerApplication(
   "todolist",
   () => import("micro-frontends-todolist"),
-  location => location.pathname.startsWith("/")
+  appFilter(["/"])
 );
 
 singleSpa.registerApplication(
   "angularapp",
   () => import("micro-frontends-weather"),
-  location => location.pathname.startsWith("/")
+  appFilter(["/"])
+);
+singleSpa.registerApplication(
+  "login",
+  () => import("micro-frontends-login"),
+  appFilter(["/login"])
 );
 
 singleSpa.start();
